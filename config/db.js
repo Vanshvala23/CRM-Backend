3904bed89c8094e8cf1836a95fa2214f67a63c35
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-01-08 13:03:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql=require('mysql2/promise');
 const dotenv=require('dotenv');
 dotenv.config();
-const db= mysql.createConnection({
+const db= mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASS,
@@ -12,9 +12,5 @@ const db= mysql.createConnection({
     queueLimit:0
 });
 
-db.connect(err=>{
-    if(err) throw err;
-    console.log("MySQL Connected");
-});
 
 module.exports =  db;
