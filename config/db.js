b421ccef460441cd4f3c9d0b429ca073d041f282
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-01-31 16:31:47 +0530

    Updated

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
   } catch (error) {
